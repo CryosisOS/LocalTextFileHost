fb2c1239de2bc370e69f2174d461fff2ef529da8
CryosisOS <nathanvdvelde@gmail.com>
2020-12-25 03:12:31 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+/**
+ * Author: Nathan van der Velde
+ * Date Created: 2020-12-25
+ * Description: This is the entry file into a proof of concept node server
+ *              for hosting text files on the localhost
+ **/
